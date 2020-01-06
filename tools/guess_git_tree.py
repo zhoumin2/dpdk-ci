b338e13817dcb78aaca18d749b20daee19e6524b
David Marchand <david.marchand@redhat.com>
2020-01-06 12:15:31 +0100

    tools: fix tree detection for orphan files
    Implicitly, if a file has no official maintainer / tree, it is in the
    end maintained in the master branch.
    
    This problem has been seen while looking at test failures reported on the
    series https://patchwork.dpdk.org/project/dpdk/list/?series=7981.
    This series mixes changes in ethdev, net/tap and EAL, and thus should go
    through the main repository.
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Acked-by: Ali Alnubani <alialnu@mellanox.com>

--- tools/guess_git_tree.py
+++ tools/guess_git_tree.py
@@ -146,8 +146,11 @@ class Maintainers(object):
         tree_list = []
         for _file in files:
             _tree = self._get_tree(_file)
-            if _tree:
-                tree_list.append(_tree)
+            # No identified tree for a file means that it should go through
+            # the main repository.
+            if not _tree:
+                _tree = 'dpdk'
+            tree_list.append(_tree)
         tree = self.get_common_denominator(tree_list)
         if tree == '':
             tree = 'dpdk'
