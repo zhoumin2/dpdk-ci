zhoumin2/dpdk-ci

commit b338e13817dcb78aaca18d749b20daee19e6524b

tools: fix tree detection for orphan files
Implicitly, if a file has no official maintainer / tree, it is in the
end maintained in the master branch.

This problem has been seen while looking at test failures reported on the
series https://patchwork.dpdk.org/project/dpdk/list/?series=7981.
This series mixes changes in ethdev, net/tap and EAL, and thus should go
through the main repository.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Ali Alnubani <alialnu@mellanox.com>

tools/guess_git_tree.py
@@ -146,8 +146,11 @@ class Maintainers(object):
         tree_list = []
         for _file in files:
             _tree = self._get_tree(_file)
-            if _tree:
-                tree_list.append(_tree)
+            # No identified tree for a file means that it should go through
+            # the main repository.
+            if not _tree:
+                _tree = 'dpdk'
+            tree_list.append(_tree)
         tree = self.get_common_denominator(tree_list)
         if tree == '':
             tree = 'dpdk'

commit fb8eab4f893334398fb202c8b05f1cf27ac4e29d

tools: revert default tree for orphan files
The problem with below patch is that some files were with no git tree in
MAINTAINERS on purpose: some files are maintained in the main repository,
but we leave subtrees touch them like release notes or makefile/meson
files.

So reverting it, and adding a comment to document the reason.

Fixes: b338e13817dc ("tools: fix tree detection for orphan files")

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Thomas Monjalon <thomas@monjalon.net>

tools/guess_git_tree.py
@@ -146,11 +146,10 @@ class Maintainers(object):
         tree_list = []
         for _file in files:
             _tree = self._get_tree(_file)
-            # No identified tree for a file means that it should go through
-            # the main repository.
-            if not _tree:
-                _tree = 'dpdk'
-            tree_list.append(_tree)
+            # Having no tree means that we accept those changes going through a
+            # subtree (e.g. release notes).
+            if _tree:
+                tree_list.append(_tree)
         tree = self.get_common_denominator(tree_list)
         if tree == '':
             tree = 'dpdk'